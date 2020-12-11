9e9569455ba7c86179da0bf1860c17cc3e818788
Tim Krase <github@timkrase.de>
2020-12-11 21:44:53 +0100

    Remove forceCoversAnnotation from phpunit

--- tests/PhpUnitCoverageBadgeTest.php
+++ tests/PhpUnitCoverageBadgeTest.php
@@ -4,11 +4,6 @@ use PHPUnit\Framework\TestCase;
 
 class PhpUnitCoverageBadgeTest extends TestCase
 {
-    /**
-     * @covers \PhpUnitCoverageBadge\CoverageReportParser::getCodeCoverage
-     * @covers \PhpUnitCoverageBadge\CoverageReportParser::getReportMetrics
-     * @covers \PhpUnitCoverageBadge\CoverageReportParser::__construct
-     */
     public function testExpectInvalidArgumentExceptionIfFileNotFound()
     {
         $coverageParser = new \PhpUnitCoverageBadge\CoverageReportParser('clover33.xml');
