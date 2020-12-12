a9e47f8fbfe449495ffed938b00fd46a3cf13c54
Tim Krase <github@timkrase.de>
2020-12-12 11:09:45 +0100

    Add github commit email and name as parameters

--- src/commit_push_badge.sh
+++ src/commit_push_badge.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-git add "${INPUT_COVERAGE_BADGE_PATH}";
-git add "${INPUT_CLOVER_REPORT}";
+git add "${INPUT_COVERAGE_BADGE_PATH}"
+git add "${INPUT_CLOVER_REPORT}"
+
+git config user.email "${INPUT_COMMIT_EMAIL}"
+git config user.name "${INPUT_COMMIT_NAME}"
 
 git commit -m "${INPUT_COMMIT_MESSAGE}"
 
