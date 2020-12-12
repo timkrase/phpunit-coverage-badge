3f7c2e002a36097250785b131c71330f15863807
Tim Krase <github@timkrase.de>
2020-12-12 11:38:02 +0100

    Fix boolean conversion

--- src/phpunit-coverage-badge.php
+++ src/phpunit-coverage-badge.php
@@ -4,7 +4,7 @@ require(__DIR__ . DIRECTORY_SEPARATOR . '../vendor/autoload.php');
 
 $cloverFile = getenv('INPUT_CLOVER_REPORT');
 $badgePath = getenv('INPUT_COVERAGE_BADGE_PATH');
-$pushBadge = getenv('INPUT_PUSH_BADGE');
+$pushBadge = filter_var(getenv('INPUT_PUSH_BADGE'), FILTER_VALIDATE_BOOLEAN);
 $repoToken = getenv('INPUT_REPO_TOKEN');
 $commitMessage = getenv('INPUT_COMMIT_MESSAGE');
 
