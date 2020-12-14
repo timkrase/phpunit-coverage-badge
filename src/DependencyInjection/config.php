ff7fa73dc41cc093b8a6ed3793f0665ee4fd78ca
Tim Krase <github@timkrase.de>
2020-12-14 23:02:00 +0100

    Fix autowiring

--- src/DependencyInjection/config.php
+++ src/DependencyInjection/config.php
@@ -1,5 +1,10 @@
 <?php declare(strict_types=1);
 
+use PhpUnitCoverageBadge\ReportParser\CloverReportParser;
+use PhpUnitCoverageBadge\ReportParser\ReportParserInterface;
+
 return [
-    ReportParserInterface::class => DI\create(\PhpUnitCoverageBadge\CloverReportParser::class)
+    ReportParserInterface::class => DI\create(CloverReportParser::class),
+    \PhpUnitCoverageBadge\BadgeGenerator::class => DI\autowire(),
+    \PhpUnitCoverageBadge\WorkflowService::class => DI\autowire()
 ];
