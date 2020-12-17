fa8e91025371c14dddc1e86f577dc28bf6684645
Tim Krase <github@timkrase.de>
2020-12-17 19:05:15 +0100

    Fix WorkflowService autowiring

--- src/DependencyInjection/config.php
+++ src/DependencyInjection/config.php
@@ -1,10 +1,12 @@
 <?php declare(strict_types=1);
 
+use PhpUnitCoverageBadge\BadgeGenerator;
 use PhpUnitCoverageBadge\ReportParser\CloverReportParser;
 use PhpUnitCoverageBadge\ReportParser\ReportParserInterface;
+use PhpUnitCoverageBadge\WorkflowService;
 
 return [
     ReportParserInterface::class => DI\create(CloverReportParser::class),
-    \PhpUnitCoverageBadge\BadgeGenerator::class => DI\autowire(),
-    \PhpUnitCoverageBadge\WorkflowService::class => DI\autowire()
+    BadgeGenerator::class => DI\autowire(),
+    WorkflowService::class => DI\autowire()
 ];
